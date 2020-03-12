c410d4934c263c587e019f552ab53e45e1fc3b17
okawa-h <ookawa124@gmail.com>
2020-03-12 14:56:32 +0900

    fix param, add scroll move

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 const POINT_LENGTH = 5;
-const RADIAN = 360;
+const SHAPE_RADIAN = 300; // 0 ~ 360
+const SHAPE_ROTATE = 0; // 0 ~ 360
+const SHAPE_RADIUS_RATIO = 1; // 0 ~ 1
 const IS_SHOW_HELPER = false;
+
 let frameList = [];
+
+class Utils {
+	static getRangeNumber(max, min) {
+		return Math.random() * (max - min) + min;
+	}
+}
 
 class FluidShapeImageFrame {
 	constructor(container) {
@@ -9,7 +18,7 @@ class FluidShapeImageFrame {
 		this.fluidShape = null;
 		this.container = container;
 		this.canvas = document.createElement('canvas');
-		this.context = this.canvas.getContext("2d");
+		this.context = this.canvas.getContext('2d');
 		this.container.appendChild(this.canvas);
 
 		this.image = this.container.getElementsByTagName('img')[0];
@@ -21,14 +30,11 @@ class FluidShapeImageFrame {
 	}
 
 	setup() {
-		const canvasWidth = this.canvas.width;
-		const canvasHeight = this.canvas.height;
-		const center = {
-			x: canvasWidth * 0.5,
-			y: canvasHeight * 0.5
-		};
-		const radius = (canvasWidth > canvasHeight ? canvasHeight : canvasWidth) * 0.5
-		this.fluidShape = new FluidShape(center, radius, POINT_LENGTH, RADIAN);
+		const width = this.canvas.width;
+		const height = this.canvas.height;
+		const center = { x: width * 0.5, y: height * 0.5 };
+		const radius = ((width > height ? height : width) * 0.5) * SHAPE_RADIUS_RATIO;
+		this.fluidShape = new FluidShape(center, radius, POINT_LENGTH, SHAPE_RADIAN, SHAPE_ROTATE);
 	}
 
 	renderer() {
@@ -44,9 +50,18 @@ class FluidShapeImageFrame {
 		this.fluidShape.draw(this.context);
 	}
 
+	showHelper() {
+		this.context.globalCompositeOperation = 'source-over';
+		this.fluidShape.drawHelper(this.context);
+	}
+
 	onResize() {
 		this.setCanvasSize();
 		this.setup();
+	}
+
+	onScroll() {
+		this.fluidShape.paramUpdate();
 	}
 
 	setCanvasSize() {
@@ -62,83 +77,106 @@ class FluidShapeImageFrame {
 }
 
 class FluidShape {
-	constructor(center, radius, pointLength, radian) {
-			this.points = [];
-			this.pointLength = pointLength;
+	constructor(center, radius, pointLength, radian, rotateWhole) {
+		this.points = [];
+		this.pointLength = pointLength;
 
-			const rota = Math.floor(radian / pointLength);
-
-			for (let i = 0; i < pointLength; i++) {
-					this.points[i] = new Point(center, radius, rota * i);
-			}
+		const rotate = Math.floor(radian / pointLength);
+		for (let i = 0; i < pointLength; i++) {
+			this.points[i] = new Point(center, radius, rotate * i + rotateWhole);
+		}
 	}
 
 	update() {
-			for (let i = 0; i < this.pointLength; i++) {
-					this.points[i].update();
-			}
+		for (let i = 0; i < this.pointLength; i++) {
+			this.points[i].update();
+		}
+	}
+
+	paramUpdate() {
+		for (let i = 0; i < this.pointLength; i++) {
+			this.points[i].paramUpdate();
+		}
 	}
 
 	draw(context) {
-		context.beginPath();
+		const lastPoint = this.points[this.pointLength - 1];
+		const xc1 = (this.points[0].x + lastPoint.x) * 0.5;
+		const yc1 = (this.points[0].y + lastPoint.y) * 0.5;
 
-		const xc1 = (this.points[0].x + this.points[POINT_LENGTH - 1].x) * 0.5;
-		const yc1 = (this.points[0].y + this.points[POINT_LENGTH - 1].y) * 0.5;
+		context.beginPath();
 		context.moveTo(xc1, yc1);
 
-		for (let i = 0; i < POINT_LENGTH - 1; i++) {
-
+		for (let i = 0; i < this.pointLength - 1; i++) {
 			const xc = (this.points[i].x + this.points[i + 1].x) * 0.5;
 			const yc = (this.points[i].y + this.points[i + 1].y) * 0.5;
 			context.quadraticCurveTo(this.points[i].x, this.points[i].y, xc, yc);
 		}
 
-		context.quadraticCurveTo(this.points[POINT_LENGTH - 1].x, this.points[POINT_LENGTH - 1].y, xc1, yc1);
+		context.quadraticCurveTo(lastPoint.x, lastPoint.y, xc1, yc1);
 		context.closePath();
 		context.fill();
+	}
 
-		if (IS_SHOW_HELPER) {
-			context.globalCompositeOperation = 'source-over';
-			for (let i = 0; i < this.pointLength; i++) {
-				this.points[i].draw(context);
-			}
+	drawHelper(context) {
+		context.globalCompositeOperation = 'source-over';
+		for (let i = 0; i < this.pointLength; i++) {
+			this.points[i].draw(context);
 		}
 	}
 }
 
 class Point {
-	constructor(center, radius, rota) {
+	constructor(center, radius, radian) {
 		this.x = null;
 		this.y = null;
-		this.centerX = center.x;
-		this.centerY = center.y;
-		this.radian = rota * (Math.PI / 180);
+		this.center = center;
 		this.radius = radius;
+		this.radian = radian * (Math.PI / 180);
 
-		this.speed = Math.random() * 5 + 1;
-		this.r = Math.random() * 1 + 1;
-		this.rota = 0;
+		this.speed = this.getSpeed();
+		this.expansionRatio = this.getExpansionRatio();
+		this.rotate = 0;
+		this.scrollState = { speed: 0, expansionRatio: 0 };
 	}
 
 	update() {
-		const plus = Math.cos(this.rota * (Math.PI / 180)) * this.r;
+		const speed = this.speed + this.scrollState.speed;
+		const expansionRatio = this.expansionRatio + this.scrollState.expansionRatio;
 
-		this.radius += plus;
+		const moveRadius = Math.cos(this.rotate * (Math.PI / 180)) * expansionRatio;
+		this.radius += moveRadius;
 
 		const cos = Math.cos(this.radian) * this.radius;
 		const sin = Math.sin(this.radian) * this.radius;
+		this.x = cos + this.center.x;
+		this.y = sin + this.center.y;
 
-		this.x = cos + this.centerX;
-		this.y = sin + this.centerY;
+		this.rotate += speed;
+		if (this.rotate > 360) this.rotate -= 360;
 
-		this.rota += this.speed;
+		this.scrollState.speed -= 0.001
+		this.scrollState.expansionRatio -= 0.001
+		if (this.scrollState.speed < 0) this.scrollState.speed = 0;
+		if (this.scrollState.expansionRatio < 0) this.scrollState.expansionRatio = 0;
+	}
 
-		if (this.rota > 360) this.rota -= 360;
+	paramUpdate() {
+		this.scrollState.speed = this.getSpeed() * 5;
+		this.scrollState.expansionRatio = this.getExpansionRatio() * 5;
+	}
+
+	getSpeed() {
+		return Utils.getRangeNumber(2, -2);
+	}
+
+	getExpansionRatio() {
+		return Utils.getRangeNumber(0.1, 0.05);
 	}
 
 	draw(context) {
 		context.beginPath();
-		context.fillStyle = 'red';
+		context.fillStyle = '#f00';
 		context.arc(this.x, this.y, 3, 0, 2 * Math.PI);
 		context.closePath();
 		context.fill();
@@ -152,18 +190,32 @@ const onInit = event => {
 	}
 	renderer();
 	window.addEventListener('resize', onResize);
+	window.addEventListener('scroll', onScroll);
 }
 
 const onResize = event => {
 	for (let i = 0; i < frameList.length; i++) {
-			frameList[i].onResize();
+		frameList[i].onResize();
+	}
+}
+
+const onScroll = event => {
+	for (let i = 0; i < frameList.length; i++) {
+		frameList[i].onScroll();
 	}
 }
 
 const renderer = () => {
 	for (let i = 0; i < frameList.length; i++) {
-			frameList[i].renderer();
+		frameList[i].renderer();
 	}
+
+	if (IS_SHOW_HELPER) {
+		for (let i = 0; i < frameList.length; i++) {
+			frameList[i].showHelper();
+		}
+	}
+
 	window.requestAnimationFrame(renderer);
 }
 
